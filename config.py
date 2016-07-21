8728c24c0bb9753a471885b907de2ca023930b17
prakharcode <srivastavaprakhar1@gmail.com>
2016-07-22 05:18:21 +0530

    github com

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-class Configuration(object):    #instructs flask to run in debug
+class Configuration(object):    #instructs flask to run in production mode
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__)) #getting current directory location
     DEBUG = False
     SECRET_KEY = 'flask is fun!'
-    db='postgres://rzxstcytozlazj:phsZ1z__CsU8-x8gjkeA_a1g7t@ec2-54-243-48-178.compute-1.amazonaws.com:5432/dfg7fns2de7j0v'
+    db= ""  #-------unique URL provided by heroku for database
     SQLALCHEMY_DATABASE_URI = db   #using the database in current directory
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     STATIC_DIR = os.path.join(APPLICATION_DIR,'static')
