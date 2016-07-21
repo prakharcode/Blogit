2742bd9d80fc99dd3d66ca30cf611c8a259e42b8
prakharcode <srivastavaprakhar1@gmail.com>
2016-07-22 05:03:08 +0530

    add admin1

--- add_entry.py
+++ add_entry.py
@@ -4,4 +4,4 @@ from app import bcrypt
 ps=bcrypt.generate_password_hash('admin')
 user=User(email='admin@gmail.com',password_hash=ps, name='Admin',admin = True)
 db.session.add(user)
-db.commit()
+db.session.commit()
