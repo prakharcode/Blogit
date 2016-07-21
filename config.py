0c860f52f6289297933f5e36420b828391a15d19
prakharcode <srivastavaprakhar1@gmail.com>
2016-07-22 04:01:16 +0530

    the url of postgresql link 1

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Configuration(object):    #instructs flask to run in debug
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__)) #getting current directory location
     DEBUG = False
     SECRET_KEY = 'flask is fun!'
-    DATABASE_URL='postgres://rzxstcytozlazj:phsZ1z__CsU8-x8gjkeA_a1g7t@ec2-54-243-48-178.compute-1.amazonaws.com:5432/dfg7fns2de7j0v'
-    SQLALCHEMY_DATABASE_URI = os.environ[DATABASE_URL]   #using the database in current directory
+    db='postgres://rzxstcytozlazj:phsZ1z__CsU8-x8gjkeA_a1g7t@ec2-54-243-48-178.compute-1.amazonaws.com:5432/dfg7fns2de7j0v'
+    SQLALCHEMY_DATABASE_URI = os.environ[db]   #using the database in current directory
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     STATIC_DIR = os.path.join(APPLICATION_DIR,'static')
     IMAGES_DIR  = os.path.join(STATIC_DIR,'images')
